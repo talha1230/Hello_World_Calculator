4e2dcc8d448fb59964abf032e91fd151fa2a9075
Talha Muhammad <121675123+talha1230@users.noreply.github.com>
2023-12-31 00:19:30 +0800

    Enhance code readability in nameConverterMainWindow class
    updated and clarity of this code; also updated UI

--- nameconvertormainwindow.cpp
+++ nameconvertormainwindow.cpp
@@ -9,58 +9,62 @@ nameconvertorMainWindow::nameconvertorMainWindow(QWidget *parent) :
 
     // Apply the style sheet to the QMainWindow
     QString mainStyle = "QMainWindow {"
-                        "    background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #87CEEB, stop:1 #f0f0f0);"
-                        "    border: 1px solid #87CEEB;"
+                        "    background-color: #2c3e50;" // Dark blue background
                         "}";
 
     this->setStyleSheet(mainStyle);
 
     // Apply the style sheet to the QPushButton
     QString buttonStyle = "QPushButton {"
-                          "    background-color: #4CAF50;"
-                          "    color: white;"
-                          "    padding: 10px 20px;"
-                          "    border: none;"
-                          "    border-radius: 5px;"
+                          "    background-color: #3498db;" // Bright blue background
+                          "    color: white;" // White text
+                          "    border: none;" // No border
+                          "    border-radius: 5px;" // Rounded corners
+                          "    padding: 5px 10px;" // Some padding
                           "}"
                           "QPushButton:hover {"
-                          "    background-color: #45A049;"
+                          "    background-color: #2980b9;" // Slightly darker blue when hovered over
                           "}"
-                          "QPushButton:focus {"
-                          "    outline: none;"
+                          "QPushButton:pressed {"
+                          "    background-color: #1a5276;" // Even darker when pressed
                           "}";
 
     ui->convertButton->setStyleSheet(buttonStyle);
 
+    // Apply the style sheet to the QLineEdit
+    QString lineEditStyle = "QLineEdit {"
+                            "    background-color: #ecf0f1;" // Light gray background
+                            "    color: ##2c3e50;" // Dark blue text
+                            "    border: none;" // No border
+                            "    border-radius: 5px;" // Rounded corners
+                            "    padding: 5px;" // Some padding
+                            "}";
+
+    ui->lineEdit_name->setStyleSheet(lineEditStyle);
+
     // Apply the style sheet to the QLabel
-    QString labelStyle = "QLabel#label_ASCIIResult {"
-                         "    background-color: #ffffff;"
-                         "    color: #333333;"
-                         "    padding: 10px;"
-                         "    border: 1px solid #4CAF50;"
-                         "    border-radius: 5px;"
+    QString labelStyle = "QLabel {"
+                         "    background-color: #ecf0f1;" // Light gray background
+                         "    color: ##2c3e50;" // Dark blue text
+                         "    border: none;" // No border
+                         "    border-radius: 5px;" // Rounded corners
+                         "    padding: 5px;" // Some padding
                          "}";
+
 
     ui->label_ASCIIResult->setStyleSheet(labelStyle);
 
+
+
     // Connect signals and slots
-    connect(ui->lineEdit_name, &QLineEdit::textChanged, this, &nameconvertorMainWindow::onConvertNameToASCII);
+    connect(ui->lineEdit_name, &QLineEdit::returnPressed, this, &nameconvertorMainWindow::onConvertButtonClicked);
     connect(ui->convertButton, &QPushButton::clicked, this, &nameconvertorMainWindow::onConvertButtonClicked);
 }
+
 void nameconvertorMainWindow::onPlainTextEditTextChangedLength() {
     // Your implementation here
 }
 
-// Rest of your implementation
-void nameconvertorMainWindow::onConvertButtonClicked() {
-    // Your implementation here
-   // QString inputValue = ui->text_form->toPlainText();
-  //  QString asciiOutput;
-  //  for (QChar character : inputValue) {
- //       asciiOutput += QString::number(character.toLatin1()) + " ";
- //   }
-}
-//
 void nameconvertorMainWindow::onConvertNameToASCII() {
     // Get the input value from the QLineEdit
     QString name = ui->lineEdit_name->text();
@@ -71,12 +75,19 @@ void nameconvertorMainWindow::onConvertNameToASCII() {
         asciiResult += QString::number(character.toLatin1()) + " ";
     }
 
-    // Update the label with the ASCII result
-    ui->label_ASCIIResult->clear();  // Clear previous text
-    ui->label_ASCIIResult->setPlainText(asciiResult);  // Use setPlainText here
+    // Prepare the final message to display
+    QString finalMessage = "" + asciiResult.trimmed();
 
-    ui->label_ASCIIResult->setPlainText("Your Name in ASCII is : " + asciiResult);
+    // Clear the label and set the new text
+    ui->label_ASCIIResult->clear();
+    ui->label_ASCIIResult->setPlainText(finalMessage);
 }
+
+void nameconvertorMainWindow::onConvertButtonClicked() {
+    // Trigger the conversion when the button is clicked
+    onConvertNameToASCII();
+}
+
 nameconvertorMainWindow::~nameconvertorMainWindow()
 {
     delete ui;
